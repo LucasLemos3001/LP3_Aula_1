1b6bd08b3518ed5723b567783f8a7783ffe3b163
LucasLemos3001 <lucassouza01ptc@gmail.com>
2026-02-06 11:35:28 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<h1>LP3 Aula 1</h1>
+<?php
+$ca = "5";
+$cb = "5";
+$sm = $ca + $cb;
+$sbtc = $ca - $cb;
+$mltp = $ca * $cb;
+$dvs = $ca / $cb;
+
+echo "a soma de $ca + $cb = $sm <br>";
+echo "a subtração de $ca - $cb = $sbtc <br>";
+echo "a multiplicação de $ca * $cb = $mltp <br>";
+echo "a divisão de $ca / $cb = $dvs <br>";
+?>
